db0ca0de0972c2635e0cab14472b18c833d8f202
madmagic007 <diegogo@telenet.be>
2020-11-20 15:33:15 +0100

    added

--- 64/Advancedgmodrpc/Config.cs
+++ 64/Advancedgmodrpc/Config.cs
@@ -19,7 +19,7 @@ namespace Advancedgmodrpc {
         public bool resetTime = true;
 
         public static void Init() {
-            DirectoryInfo dir = Directory.CreateDirectory(Environment.GetEnvironmentVariable("APPDATA") + "/MadMagic/Advanced gmod RPC");
+            DirectoryInfo dir = Directory.CreateDirectory(Environment.GetEnvironmentVariable("APPDATA") + "/MadMagic/Advanced gmod RPC (x64)");
 
             configFile = new FileInfo(dir.FullName + "/GmodRPC.mccfg");
             if (!configFile.Exists) configFile.Create();
@@ -28,11 +28,11 @@ namespace Advancedgmodrpc {
                 byte[] data = File.ReadAllBytes(configFile.FullName);
                 MemoryStream ms = new MemoryStream(data);
                 config = (Config)new BinaryFormatter().Deserialize(ms);
-            } catch (Exception) {}
+            } catch (Exception) { config = new Config(); }
 
 
             unmappedFile = new FileInfo(dir.FullName + "/unmappedGamemodes.txt");
-            if (!unmappedFile.Exists) unmappedFile.Create(); config = new Config();
+            if (!unmappedFile.Exists) unmappedFile.Create();
             unmappedGamemodes = File.ReadAllText(unmappedFile.FullName);
         }
 
@@ -41,12 +41,14 @@ namespace Advancedgmodrpc {
             new BinaryFormatter().Serialize(ms, config);
             byte[] data = ms.ToArray();
 
+            if (!configFile.Exists) configFile.Create();
             File.WriteAllBytes(configFile.FullName, data);
         }
 
         public static void AppendGamemode(string gamemode) {
             if (unmappedGamemodes.Contains(gamemode)) return;
             unmappedGamemodes += gamemode + "\n";
+            if (!unmappedFile.Exists) unmappedFile.Create();
             File.WriteAllText(unmappedFile.FullName, unmappedGamemodes);
         }
     }
